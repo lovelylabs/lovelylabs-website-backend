2042cc4daf1e22b18060fe7a1b194424258e0e29
Alexander Guerra <alex@lovelylabs.io>
2020-09-14 02:17:16 -0500

    Mass commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
-const PORT = process.env.PORT || 5000
+const fs = require('fs')
+const path = require('path')
+const PORT = process.env.PORT || 5001
 const app = express()
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/resume', (request, response) => {
+    response.json({ name: 'Alexander Guerra' })
 })
 
 app.listen(PORT, () => {
