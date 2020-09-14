7d842c2ffd2cf81ff6a610d6970c8b6b938cf160
Alexander Guerra <alex@lovelylabs.io>
2020-09-14 02:39:26 -0500

    Update resume.json

--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ const path = require('path')
 const PORT = process.env.PORT || 5001
 const app = express()
 
+const getResume = async (request, response, next) => {
+    try {
+        const data = fs.readFileSync(path.join(__dirname, '../data/resume.json'))
+        const resume = JSON.parse(data)
+        response.json(resume)
+    } catch (error) {
+        console.log('There was an error: ', error)
+    }
+}
+
 app.get('/hello', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/resume', (request, response) => {
-    response.json({ name: 'Alexander Guerra' })
-})
+app.get('/resume', getResume)
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
