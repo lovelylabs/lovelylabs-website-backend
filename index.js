13355df45ed4879b200c6f1e8745a3466b734484
Alexander Guerra <alex@lovelylabs.io>
2020-09-14 01:21:55 -0500

    Make modification to index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const PORT = process.env.PORT || 5000
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
     res.send('Hello World!')
 })
 
